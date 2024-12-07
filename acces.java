da3c40bc81f58cb1e575596fbd00eca441f4f2f3
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-07 14:32:27 +0530

    Add files via upload

---
+++ acces.java
@@ -0,0 +1,8 @@
+public class acces {
+    public static void main(String[] args){
+        int arr[]={22,44,56,67,78,79};
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+    }
+    
+}
