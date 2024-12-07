77ee01a77e021a2346c1946d67dcf8e7b0d0d906
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-07 14:31:48 +0530

    Add files via upload

---
+++ init.java
@@ -0,0 +1,9 @@
+ import java.util.*;
+ public class init{
+    public static void main(String[] args){
+        int arr[]={22,44,55,77,88,99};
+        System.out.println(Arrays.toString(arr));
+        
+    }
+    
+}
