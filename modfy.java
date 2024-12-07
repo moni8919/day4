9b6d0800e7315dbcce28b97d5e1cd0061986a556
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-07 14:32:07 +0530

    Add files via upload

---
+++ modfy.java
@@ -0,0 +1,10 @@
+ import java.util.*;
+ public class modfy {
+    public static void main(String[] args){
+        int arr[]={22,33,45,67,79};
+        arr[2]=456;
+        arr[3]=79;
+        System.out.println(Arrays.toString(arr));
+    }
+    
+}
