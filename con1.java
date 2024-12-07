76c978d8e3b2e112baf3e03b9b0b4049508881d0
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-07 11:36:45 +0530

    Add files via upload

---
+++ con1.java
@@ -0,0 +1,14 @@
+
+ import java.util.*;
+ public class con1{
+    public static void main(String[] args){
+    {
+      for(int i=0;i<5;i++){
+        if(i==2)
+        continue;
+        System.out.println("continue example");
+        System.out.println(i);
+      }
+    }
+ }  
+}
