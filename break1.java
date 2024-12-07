9b4d4f99c18f886a14b80fe7a65b02359b11f3fd
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-07 11:35:30 +0530

    Add files via upload

---
+++ break1.java
@@ -0,0 +1,12 @@
+public class break1 {
+    public static void main(String[] args)
+    {
+      for(int i=0;i<5;i++){
+        if(i==2)
+        break;
+        System.out.println("break example");
+        System.out.println(i);
+      }
+    }
+    
+}
