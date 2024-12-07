a054557d000959b4bfe1087ff138867187461051
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-07 11:35:49 +0530

    Add files via upload

---
+++ but.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class but{
+    public static void main(String[] args){
+        int n=9;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(j==0 || i==j || j==n-1 || i+j==n-1)
+                System.out.print("*");
+                else 
+                System.out.print(" ");
+            }
+            System.out.println();
+    }    }
+} 
+    
